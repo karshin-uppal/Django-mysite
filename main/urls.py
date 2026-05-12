c2d48a79a789c625dbd5e777a2dd10c7e31c1dd5
karshin-uppal <dev4@dipanetech.com>
2026-05-12 14:58:46 +0530

    Implement basic Django app structure with models, views, and templates

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views #. means current directory, so we are importing views.py from the current directory
 
 urlpatterns = [
-    path("<str:name>/", views.index, name = "index"),
+    path("<int:id>/", views.index, name = "index"),
+    path("", views.home, name="home")
 ]
--- main/views.py
+++ main/views.py
@@ -3,10 +3,9 @@ from django.http import HttpResponse
 from .models import ToDoList, Item
 # Create your views here.
 
-def index(response, name):
-    ls = ToDoList.objects.filter(name=name).first()
-    item = ls.item_set.get(id=1)
-    return HttpResponse("<h1> %s </h1><br></br><p>%s</p>" %(ls.name, str(item.text)))
-    # return HttpResponse("<h1>%s</h1>" %ls.name)
+def index(response, id):
+    ls = ToDoList.objects.get(id=id)
+    return render(response, "main/base.html", {"name": ls.name})
 
-
+def home(response):
+    return render(response, "main/home.html", {"name": "Test"})
