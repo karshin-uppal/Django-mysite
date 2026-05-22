a99da4d4183f9dbafe2afa915dc083b84fefe3c6
karshin-uppal <dev4@dipanetech.com>
2026-05-22 13:09:43 +0530

    Add create list functionality with forms and templates

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from . import views #. means current directory, so we are importing views.py fro
 
 urlpatterns = [
     path("<int:id>/", views.index, name = "index"),
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("create/", views.create, name="create")
 ]
--- main/views.py
+++ main/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ToDoList, Item
+from .forms import CreateNewList
 # Create your views here.
 
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
-    return render(response, "main/base.html", {"name": ls.name})
+    return render(response, "main/list.html", {"ls": ls})
 
 def home(response):
     return render(response, "main/home.html", {"name": "Test"})
+
+
+def create(response):
+    form = CreateNewList() 
+    return render(response, "main/create.html", {"form": form})
